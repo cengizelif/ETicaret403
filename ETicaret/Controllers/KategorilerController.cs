07723241d013630e5f9585a313566a06a91f26de
PC <PC@DESKTOP-SG7CU1Q>
2022-04-11 12:44:04 +0300

    Add project files.

---
+++ ETicaret/Controllers/KategorilerController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ETicaret.Models;
+
+namespace ETicaret.Controllers
+{
+    public class KategorilerController : Controller
+    {
+        private ETicaretEntities db = new ETicaretEntities();
+
+        // GET: Kategoriler
+        public ActionResult Index()
+        {
+            return View(db.Kategoriler.ToList());
+        }
+
+        // GET: Kategoriler/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            if (kategoriler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoriler);
+        }
+
+        // GET: Kategoriler/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "KategoriId,KategoriAdi")] Kategoriler kategoriler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kategoriler.Add(kategoriler);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kategoriler);
+        }
+
+        // GET: Kategoriler/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            if (kategoriler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoriler);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "KategoriId,KategoriAdi")] Kategoriler kategoriler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kategoriler).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kategoriler);
+        }
+
+        // GET: Kategoriler/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            if (kategoriler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoriler);
+        }
+
+        // POST: Kategoriler/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            db.Kategoriler.Remove(kategoriler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    
+    }
+}
