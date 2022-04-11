b5d72ec0a90ba6248b6b18f85ab845677f6408b5
cenelif@gmail.com <cenelif@gmail.com>
2022-04-11 14:43:33 +0300

    Kategori ve ürünlerde değişiklik

---
+++ ETicaret/Controllers/UrunlerController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ETicaret.Models;
+
+namespace ETicaret.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        private ETicaretEntities db = new ETicaretEntities();
+
+        // GET: Urunler
+        public ActionResult Index()
+        {
+            var urunler = db.Urunler.Include(u => u.Kategoriler);
+            return View(urunler.ToList());
+        }
+
+        // GET: Urunler/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urunler urunler = db.Urunler.Find(id);
+            if (urunler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(urunler);
+        }
+
+        // GET: Urunler/Create
+        public ActionResult Create()
+        {
+            ViewBag.KategoriId = new SelectList(db.Kategoriler, "KategoriId", "KategoriAdi");
+            return View();
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UrunId,UrunAdi,UrunAciklamasi,UrunFiyati,KategoriId")] Urunler urunler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Urunler.Add(urunler);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.KategoriId = new SelectList(db.Kategoriler, "KategoriId", "KategoriAdi", urunler.KategoriId);
+            return View(urunler);
+        }
+
+        // GET: Urunler/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urunler urunler = db.Urunler.Find(id);
+            if (urunler == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.KategoriId = new SelectList(db.Kategoriler, "KategoriId", "KategoriAdi", urunler.KategoriId);
+            return View(urunler);
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UrunId,UrunAdi,UrunAciklamasi,UrunFiyati,KategoriId")] Urunler urunler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(urunler).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.KategoriId = new SelectList(db.Kategoriler, "KategoriId", "KategoriAdi", urunler.KategoriId);
+            return View(urunler);
+        }
+
+        // GET: Urunler/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urunler urunler = db.Urunler.Find(id);
+            if (urunler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(urunler);
+        }
+
+        // POST: Urunler/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Urunler urunler = db.Urunler.Find(id);
+            db.Urunler.Remove(urunler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+      
+    }
+}
