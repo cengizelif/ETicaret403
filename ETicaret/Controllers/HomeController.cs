84fa32ec45d2d679582b07f17c88aee12ce1a5a2
cenelif@gmail.com <cenelif@gmail.com>
2022-04-12 12:52:17 +0300

    Kategoriye göre Ürün

--- ETicaret/Controllers/HomeController.cs
+++ ETicaret/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ETicaret.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,22 +9,24 @@ namespace ETicaret.Controllers
 {
     public class HomeController : Controller
     {
+        private ETicaretEntities db = new ETicaretEntities();
         public ActionResult Index()
         {
+            ViewBag.KategoriListesi = db.Kategoriler.ToList();
+            ViewBag.SonUrunler = db.Urunler.OrderByDescending(x => x.UrunId).Take(10).ToList();
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Kategori(int id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            ViewBag.KategoriListesi = db.Kategoriler.ToList();
+            ViewBag.Kategori = db.Kategoriler.Find(id);
+            return View(db.Urunler.Where(x=>x.KategoriId==id).ToList());
         }
 
-        public ActionResult Contact()
+        public ActionResult Urun()
         {
-            ViewBag.Message = "Your contact page.";
-
+       
             return View();
         }
     }
