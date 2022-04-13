9632892ce063284d23bc636a062c0b901939eb5e
cenelif@gmail.com <cenelif@gmail.com>
2022-04-13 11:03:46 +0300

    Sepet işlemleri

--- ETicaret/Controllers/SepetController.cs
+++ ETicaret/Controllers/SepetController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,7 +16,10 @@ namespace ETicaret.Controllers
   
         public ActionResult Index()
         {
-            return View();
+            string userID = User.Identity.GetUserId();
+            var sepet = db.Sepet.Where(x => x.UserId == userID).Include(s=>s.Urunler);
+
+            return View(sepet.ToList());
         }
         public ActionResult SepeteEkle(int? adet,int id)
         {
@@ -43,6 +48,36 @@ namespace ETicaret.Controllers
 
             db.SaveChanges();
 
+            return RedirectToAction("Index");
+        }
+   
+        public ActionResult SepetGuncelle(int? adet,int id)
+        {
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sepet sepet = db.Sepet.Find(id);
+            if(sepet==null)
+            {
+                return HttpNotFound();
+            }
+
+            Urunler urun = db.Urunler.Find(sepet.UrunId);
+
+            sepet.Adet = adet ?? 1;
+            sepet.ToplamTutar = sepet.Adet * urun.UrunFiyati;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Sil(int id)
+        {
+            Sepet sepet = db.Sepet.Find(id);
+            db.Sepet.Remove(sepet);
+            db.SaveChanges();
             return RedirectToAction("Index");
         }
     }
